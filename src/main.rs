2575b80e12f5ea090cca9cd493c62d9fac2dffc3
tomoya ishigaki <steinwand.tomo@gmail.com>
2023-04-04 23:55:55 +0900

    feat: add basic server to handle requests on localhost:7878

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, net::TcpListener};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
+    for stream in listener.incoming() {
+        if let Ok(stream) = stream {
+            println!("{:?}", stream);
+        } else {
+            println!("Something is faild.");
+        }
+    }
+    Ok(())
 }
