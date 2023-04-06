e657e636af1ecaad1004f8a5f8889cf4d75d41ce
tomoya ishigaki <steinwand.tomo@gmail.com>
2023-04-06 22:18:07 +0900

    feat: implement displaying requests

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
-use std::{error::Error, net::TcpListener};
+use std::{
+    error::Error,
+    io::Read,
+    net::{TcpListener, TcpStream},
+};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind("127.0.0.1:7878")?;
     for stream in listener.incoming() {
         if let Ok(stream) = stream {
-            println!("{:?}", stream);
+            handle_connection(stream);
         } else {
             println!("Something is faild.");
         }
     }
     Ok(())
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf = [0; 1024];
+
+    match stream.read(&mut buf) {
+        Ok(_) => println!("Request: {}", String::from_utf8_lossy(&buf[..])),
+        Err(e) => println!("{e}"),
+    }
 }
